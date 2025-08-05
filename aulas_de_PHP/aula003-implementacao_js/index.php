4d4705375e35458d2ce11b60ec6fe07cd220ec3a
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-08-05 20:19:25 -0300

    Arrumado

--- aulas_de_PHP/aula003-implementacao_js/index.php
+++ aulas_de_PHP/aula003-implementacao_js/index.php
@@ -9,9 +9,9 @@
     <h1>Envie sua mensagem</h1>
 
     <div class="caixa">
-        <form id="meuFormulario" method="POST" action="precesso.php">
+        <form id="meuFormulario" method="POST" action="processo.php">
             <label>Digite sua mensagem:</label>
-            <input type="text" id="mensagem" nome="mensagem" required><br>
+            <input type="text" id="mensagem" name="mensagem" required><br>
             <button type="submit">Enviar</button>
         </form>
     </div>
