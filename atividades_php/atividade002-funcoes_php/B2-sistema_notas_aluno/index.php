04efe1f9899e94705c358280e6f6e05e8c1ec11c
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-08-12 21:37:34 -0300

    B2 - fazer php

---
+++ atividades_php/atividade002-funcoes_php/B2-sistema_notas_aluno/index.php
@@ -0,0 +1,49 @@
+<!-- 2. Sistema de Notas de Alunos
+Crie um programa que leia o nome de um aluno e quatro notas.
+
+Crie uma função que calcule a média e retorne se o aluno está Aprovado, em Recuperação ou Reprovado.
+Use estruturas condicionais para determinar o resultado.
+Permita que o programa cadastre vários alunos, mostrando o resultado individual de cada um. -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Notas de Alunos</title>
+    <link rel="stylesheet" href="public/css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Sistema de Notas de Alunos</h1>
+    </header>
+
+    <main>
+        <div>
+            <form class="formulario" method="POST" action="public/processo.php">
+
+                <label for="nomeAluno">Digite o nome do aluno</label><br>
+                <input id="nomeAluno" type="text" name="nomeAluno" required>
+
+                <div id="notas">
+
+                    <label for="nota1">Digite a 1ª nota</label><br>
+                    <input id="nota1" type="number" name="entrada" required>
+                    
+                    <label for="nota2">Digite a 2ª nota</label><br>
+                    <input id="nota2" type="number" name="entrada" required>
+                    
+                    <label for="nota3">Digite a 3ª nota</label><br>
+                    <input id="nota3" type="number" name="entrada" required>
+                    
+                    <label for="nota4">Digite a 4ª nota</label><br>
+                    <input id="nota4" type="number" name="entrada" required>
+                    
+                </div>
+                    <button type="submit">Enviar</button>
+            </form>
+        </div>
+    </main>
+
+    <script src="assets/js/validacao.js"></script>
+</body>
+</html>
---
+++ atividades_php/atividade002-funcoes_php/B2-sistema_notas_aluno/public/processo.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Notas de Alunos</title>
+    <link rel="stylesheet" href="public/css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Sistema de Notas de Alunos</h1>
+    </header>
+
+    <main>
+        <form class="formulario" method="POST" action="public/processo.php">
+        <?php
+
+        ?>
+        </form>
+    </main>
+
+    <script src="assets/js/validacao.js"></script>
+</body>
+</html>
