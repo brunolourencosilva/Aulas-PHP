d113e76e2cbb1158fc4c7c672535926d344a33d2
brunolourencosilva <brunolourenco.silva15@gmail.com>
2025-08-14 15:24:47 -0300

    D4 - impossiverl ser feita  sem array

--- atividades_php/atividade002-funcoes_php/D4-sistema_login/index.php
+++ atividades_php/atividade002-funcoes_php/D4-sistema_login/index.php
@@ -1,3 +1,10 @@
+<!-- Implemente um programa de login em PHP.
+
+Armazene um pequeno array com usuários e senhas.
+Crie uma função para verificar se o login e a senha estão corretos.
+Use estruturas condicionais para validar o acesso.
+Permita três tentativas de login antes de encerrar o programa. -->
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
