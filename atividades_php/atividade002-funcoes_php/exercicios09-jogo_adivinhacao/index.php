e33bbf6fbd951b01818443adc9e32ea28b8e257f
brunolourencosilva <brunolourenco.silva@gmail.com>
2025-08-19 21:34:28 -0300

    fazer session

--- atividades_php/atividade002-funcoes_php/exercicios09-jogo_adivinhacao/index.php
+++ atividades_php/atividade002-funcoes_php/exercicios09-jogo_adivinhacao/index.php
@@ -1,53 +1,31 @@
+<!-- Crie um jogo onde o computador gera um número aleatório de 1 a 100 e o usuário deve adivinhar.
+
+Crie uma função que valide o palpite e retorne se o número é maior, menor ou correto.
+Use laços de repetição para permitir várias tentativas.
+Mostre ao final quantas tentativas o jogador precisou para acertar. -->
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Exemplo de estruturas de repetição em PHP</title>
+    <title>Jogo de Adivinhação</title>
     <link rel="stylesheet" href="public/css/estilo.css">
 </head>
 <body>
     <header>
-        <h1>Exemplo de estruturas de Repetição</h1>
+        <h1>Jogo de Adivinhação</h1>
     </header>
 
     <main>
-        <section>
-            <h2>Estrutura FOR</h2>
-            <div class="caixa">
-                <?php
-                    // incluie o arquivo PHP que contem as funções
-                    // em seguida,executa a função que exibe o loop FOR
-                    include "public/processo.php";
-                    exibirFor();
-                ?>
-            </div>
-        </section>
-        <!-- Seção que exibe loop WHILE -->
-        <section>
-            <h2>Estrutura WHILE</h2>
-            <div class="caixa">
-                <?php
-                    // Chama a função que exibe o loop wHILE
-                    exibirWhile();
-                ?>
-            </div>
-        </section>
+        <div>
+            <form class="formulario" method="POST" action="public/processo.php">
+                <label for="palpite">Digite um palpite entre 0 a 100:</label><br>
+                <input type="number" name="palpite" required>
 
-        <!-- Seção que exibe loop FOREACH -->
-        <section>
-            <h2>Estrutura FOREACH</h2>
-            <div class="caixa">
-                <?php
-                    // Chama a função que exibe o loop FOREACH
-                    exibirForeach();
-                ?>
-            </div>
-        </section>
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
     </main>
-
-    <footer>
-        <p>Exemplo didatico de PHP &copy; 2025</p>
-    </footer>
 </body>
 </hmlt>
 
